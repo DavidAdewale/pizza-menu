99d1f367d5c6b7bda276cc336ea0a7bf54eff76b
unknown <adewaledavidola@gmail.com>
2023-06-25 14:38:51 +0100

    final commit

--- src/index.js
+++ src/index.js
@@ -79,25 +79,22 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>There are no pizzas available 😢 </p>
       )}
-
-      {/* <Pizza
-        name="Pizza Spinachi"
-        ingredients="Tomato, mozarella, and pepperoni"
-        photoName="pizzas/spinaci.jpg"
-        price={10}
-      /> */}
     </main>
   );
 }
 
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
@@ -112,22 +109,31 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 8;
+  const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
 
   return (
     <footer className="footer">
-      {isOpen && (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online</p>
-          <button className="btn">Order</button>
-        </div>
+      {isOpen ? (
+        <Order close={closeHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00
+        </p>
       )}
     </footer>
   );
 }
 
+function Order(props) {
+  return (
+    <div className="order">
+      <p>We're open until {props.close}:00. Come visit us or order online</p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
